1d3a3c308e2443fa3b3f725dd79e54f7e02c8057
Arnaud Debray <debray.arnaud@gmail.com>
2014-08-16 22:56:35 +0200

    save up

--- web/calendar.dart
+++ web/calendar.dart
@@ -2,29 +2,17 @@ part of citadel;
 
 class Calendar
 {
-	List<Map>	datalist;
+	Map			data;
 	Completer	_completer;
 
 	Future onLoad()
 	{
 		_completer = new Completer();
-		datalist = new List();
 		HttpRequest.getString('data/calendar.json')
 			.then( (String string)
 			{
-				int			i;
-				Map			data;
-
-				i = 0;
-				string.split('\n').forEach( (elem)
-				{
-					if (elem[0] != '{' && elem[0] != '}')
-					{
-						data = JSON.decode('{' + elem.replaceFirst(',', '') + '}');
-						datalist.add(data);
-					}
-					i += 1;
-				});
+				data = JSON.decode(string);
+				print(data);
 				_completer.complete(this);
 			})
 			.catchError( (error)
